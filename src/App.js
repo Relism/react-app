2c294367df7e600e4cd796e09ae17501769dc62e
Zakaria El Orche <relismdev@gmail.com>
2022-11-05 18:44:30 +0000

    Update App.js and index.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Imperial Circus Decadence</h1>
       <h2>Halo!</h2>
     </div>
   );
